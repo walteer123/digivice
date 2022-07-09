841e5f20c9872c4c2d50b4108cf61c839fbaf0e0
Walter <walter_rfl@hotmail.com>
2022-07-09 17:06:08 -0300

    simple image/text loading

--- features/digimons/src/main/java/com/walter/digivice/digimons/presentation/DigimonsListScreen.kt
+++ features/digimons/src/main/java/com/walter/digivice/digimons/presentation/DigimonsListScreen.kt
@@ -1,12 +1,20 @@
 package com.walter.digivice.digimons.presentation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.walter.digivice.digimons.domain.entity.Digimon
 
 @Composable
@@ -15,8 +23,8 @@ fun DigimonsListScreen(
 ) {
     val state = digimonViewModel.state.collectAsState().value
 
-    LazyColumn {
-        items(state.data){ item ->
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+        items(state.data) { item ->
             DigimonRow(digimon = item)
         }
     }
@@ -25,7 +33,14 @@ fun DigimonsListScreen(
 
 @Composable
 fun DigimonRow(digimon: Digimon) {
-    Text(text = digimon.name)
+    Row() {
+        AsyncImage(
+            model = digimon.img, contentDescription = "Digimon Image",
+            modifier = Modifier.clip(CircleShape)
+        )
+        Text(text = digimon.name)
+    }
+
 }
 
 
