c1a0f45cf8031ada140be57ddd1b5f446b116ee0
Walter <walter_rfl@hotmail.com>
2022-08-17 20:08:36 -0300

    init DigimonDetailsScreen.kt

---
+++ features/digimon-details/src/main/java/com/walter/digimon_details/presentation/DigimonDetailsScreen.kt
@@ -0,0 +1,12 @@
+package com.walter.digimon_details.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun DigimonDetailsScreen() {
+    Column {
+
+    }
+}
